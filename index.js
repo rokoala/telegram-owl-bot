f982d6e190f84b56e041cc6b28b6c282f01aed2b
Rodrigo Koga <koga.rodrigo@gmail.com>
2017-08-15 12:00:41 -0300

    :)

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
 const winston = require('winston');
 const TelegramBot = require('node-telegram-bot-api');
 
-winston.add(winston.transports.File, {filename:'bot.log'})
-winston.remove(winston.transports.Console);
+winston.add(winston.transports.File, {
+  filename: 'bot.log'
+});
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = require('./env.json').API_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-  winston.log('info',msg);
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+const bot = new TelegramBot(token, {
+  polling: true
 });
 
-bot.onText(/\/getrandomquestion/, (msg, match) =>{
-  bot.sendMessage(msg.chat.id, "O que é o que é caí em pé e corre deitado ?")
-  bot.sendMessage(msg.chat.id, "A. Cadeira\nB. Chuva\n C.Fogo \n D.Vento", {
-    "reply_markup":{
-      "keyboard":[["a","b"],["c","d"]]
+bot.onText(/\/getrandomquestion/, (msg, match) => {
+  bot.sendMessage(msg.chat.id, "O que é o que é caí em pé e corre deitado?\nA. Cadeira\nB. Chuva\n C.Fogo \n D.Vento", {
+    "reply_markup": {
+      "keyboard": [
+        ["a", "b"],
+        ["c", "d"]
+      ]
     }
-  });
+  }).then((obj)=>{
+    console.log(obj);
+    // bot.onReplyToMessage(chatId, messageId, callback)
+  })
 })
-
-bot.on("callback_query", (callbackQuery)=>{
-  winston.log('info',callbackQuery);
-})
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
 
 winston.info('Bot started')
